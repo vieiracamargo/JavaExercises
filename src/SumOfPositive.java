b59438fc37b3ebbf0332e5f316d1bc94480f5eeb
vieiracamargo <brunovieiracamargo@gmail.com>
2022-11-23 21:19:27 -0300

    sum of all of the positives numbers of array

--- src/SumOfPositive.java
+++ src/SumOfPositive.java
@@ -1,2 +1,18 @@
 public class SumOfPositive {
+//    You get an array of numbers, return the sum of all of the positives ones.
+//    Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//    Note: if there is nothing to sum, the sum is default to 0.
+    public static void main(String[] args) {
+        int[] arr = {1,-4,7,12};
+        System.out.println(sum(arr));
+    }
+
+    public static int sum(int[] arr){
+        int sum = 0;
+        for(int element: arr){
+            if( element > 0) sum += element;
+        }
+        return sum;
+    }
+
 }
