3f58273059593753d10a1825f41681883b2bdad2
vieiracamargo <brunovieiracamargo@gmail.com>
2022-11-28 21:57:09 -0300

    counts the number of sheep present in the array

--- src/CountingSheep.java
+++ src/CountingSheep.java
@@ -1,5 +1,5 @@
 public class CountingSheep {
-    //Consider an array/list of sheep where some sheep may be missing from their place.
+//Consider an array/list of sheep where some sheep may be missing from their place.
 //We need a function that counts the number of sheep present in the array (true means present).
 //Hint: Don't forget to check for bad values like null/undefined
     public static void main(String[] args) {
