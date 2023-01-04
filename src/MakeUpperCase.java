8651beb2be514a6a50dee72b4c64138585e52564
vieiracamargo <brunovieiracamargo@gmail.com>
2023-01-04 20:23:36 -0300

    makeUpperCase

---
+++ src/MakeUpperCase.java
@@ -0,0 +1,10 @@
+public class MakeUpperCase {
+    public static void main(String[] args) {
+// Write a function which converts the input string to uppercase.
+        System.out.println(makeUpperCase("bruno"));
+    }
+
+    public static String makeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
